33864bed990c97711c7d53dd1aa82af6481d7a9d
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-02-27 17:18:50 +0000

    More tests (#51)
    * bump pre-commit/mirrors-eslint to fix false-pos undefined vars from {@const} destructuring
    
    * document all props in readme (removeBtnTitle, removeAllTitle, defaultDisabledTitle were missing)
    
    * test all props are mentioned in readme
    
    * add CSS resets for margin + padding of input + button
    
    * add test for remove all button (probably brittle), need to figure out how to isolate test each into its own DOM

--- tests/multiselect.test.ts
+++ tests/multiselect.test.ts
@@ -1,12 +1,15 @@
-import { fireEvent, render } from '@testing-library/svelte'
-import { expect, test } from 'vitest'
+import { cleanup, fireEvent, render } from '@testing-library/svelte'
+import { readFileSync } from 'fs'
+import { afterEach, expect, test } from 'vitest'
 import MultiSelect from '../src/lib'
 
 const options = [`Banana`, `Watermelon`, `Apple`, `Dates`, `Mango`]
 const placeholder = `Select fruits`
 
+afterEach(cleanup)
+
 test(`can focus input, enter text, toggle hidden options and select an option`, async () => {
-  const { getByPlaceholderText, getByText, container } = render(MultiSelect, {
+  const { getByText, container, getByPlaceholderText } = render(MultiSelect, {
     options,
     placeholder,
   })
@@ -15,7 +18,7 @@ test(`can focus input, enter text, toggle hidden options and select an option`, 
 
   expect(ul_ops?.classList.contains(`hidden`)).to.equal(true)
 
-  const input = getByPlaceholderText(`Select fruits`)
+  const input = getByPlaceholderText(placeholder)
   await fireEvent.focus(input)
   await fireEvent.input(input, { target: { value: `Apple` } })
 
@@ -32,6 +35,48 @@ test(`can focus input, enter text, toggle hidden options and select an option`, 
 
   const apple_sel = getByText(`Apple`, { selector: `ul.selected > li` })
   expect(apple_sel.textContent?.trim()).toBe(`Apple`)
+})
+
+test(`readme documents all props`, () => {
+  const readme = readFileSync(`readme.md`, `utf8`)
+
+  const instance = new MultiSelect({
+    target: document.body,
+    props: { options },
+  })
+
+  for (const prop of Object.keys(instance.$$.props)) {
+    expect(readme).to.contain(prop)
+  }
+})
+
+test(`remove all button`, async () => {
+  const { container, getByPlaceholderText } = render(MultiSelect, {
+    options,
+    placeholder,
+  })
+
+  const input = getByPlaceholderText(placeholder)
+  await fireEvent.focus(input)
+
+  const ul_ops = container.querySelector(`ul.options`)
+
+  expect(ul_ops?.children.length).toBe(options.length)
+
+  const li_ops = container.querySelector(`ul.options`)?.children
+  for (const li of li_ops ?? []) {
+    await fireEvent.mouseDown(li)
+  }
+
+  const ul_sel = container.querySelector(`ul.selected`)
+  // make sure all options are selected
+  expect(ul_sel?.textContent).toContain(`Mango Apple Banana`)
+
+  const rm_all_btn = container.querySelector(`button[title='Remove all']`)
+  await fireEvent.mouseUp(rm_all_btn)
+
+  const ul_sel_after = container.querySelector(`ul.selected`)
+  expect(ul_sel_after?.textContent).toBe(` `) // only input left
 })
 
 test(`default export from index.ts is same as component file`, async () => {
