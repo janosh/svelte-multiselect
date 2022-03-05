dcf2877ebd6a76b78cefe25ba939bf6b5aa90be9
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-03-05 20:39:12 +0000

    set width (not height) on svg icons and as px (not em) so they don't shrink with fluid typography on mobile screens

--- src/lib/icons/ChevronExpand.svelte
+++ src/lib/icons/ChevronExpand.svelte
@@ -1,10 +1,4 @@
-<script lang="ts">
-  export let width: string | number = `1em`
-  export let height: string | number = width
-  export let style = ``
-</script>
-
-<svg {width} {height} {style} fill="currentColor" viewBox="0 0 16 16">
+<svg {...$$props} fill="currentColor" viewBox="0 0 16 16">
   <path
     d="M3.646 9.146a.5.5 0 0 1 .708 0L8 12.793l3.646-3.647a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 0-.708zm0-2.292a.5.5 0 0 0 .708 0L8 3.207l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 0 0 0 .708z"
   />
