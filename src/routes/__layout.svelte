0cad5eaeafae591d292968e1db138e5af31b0cdd
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-04-07 18:42:25 +0100

    Several fixes for `allowUserOptions` (#69)
    * pre-commit set default_install_hook_types: [pre-commit, commit-msg]
    
    * prevent form submission on enter key
    
    * only create new labels with truthy allowUserOptions on enter when user entered text before
    
    * fix error "MultiSelect: option with label test not found" https://github.com/janosh/svelte-multiselect/pull/67#issuecomment-1086867925
    
    * add src/routes/demos/allow-user-options.svx
    
    * add 2 tests for allowUserOptions (true and append mode)
    
    * add local anti test.only( commit hook
    
    * MultiSelect new prop addOptionMsg
    
    * readme document addOptionMsg
    
    * add test 'shows custom addOptionMsg if no options match'
    
    * fix test 'multiselect > can select and remove many options'
    
    * update deps
    
    * reduce route nesting
    
    * dropdown show/hide transition replace keyed svelte fly with CSS-only transition
    
    * fix test 'external CSS classes > attaches to correct DOM node' by refocusing input on selection (unless maxSelect reached)
    
    also increase timeout for 'allowUserOptions > shows custom addOptionMsg if no options match' to make less brittle

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,75 @@
 <script lang="ts">
   import GitHubCorner from 'svelte-github-corner'
   import '../app.css'
+  import { page } from '$app/stores'
+
+  const demo_routes = Object.keys(import.meta.glob(`./*.svx`)).map(
+    (filename) => filename.split(`.`)[1]
+  )
+
+  $: isCurrent = (path: string) => {
+    if (path === $page.url.pathname) return `page`
+    if (path !== `/` && $page.url.pathname.includes(path)) return `page`
+    return undefined
+  }
 </script>
 
 <GitHubCorner href="https://github.com/janosh/svelte-multiselect" />
 
-<slot />
+{#if demo_routes.includes($page.url.pathname)}
+  <a href="/" sveltekit:prefetch aria-label="Back to index page">&laquo; back</a>
+  <h1>
+    <img src="/favicon.svg" alt="Svelte MultiSelect" height="50" width="50" />&ensp;Svelte
+    MultiSelect
+  </h1>
+
+  <p>Other examples</p>
+  <nav>
+    {#each demo_routes as route, idx}
+      {#if idx > 0}<strong>&bull;</strong>{/if}
+      <a href={route} sveltekit:prefetch aria-current={isCurrent(route)}>{route}</a>
+    {/each}
+  </nav>
+
+  <main>
+    <slot />
+  </main>
+{:else}
+  <!-- handles non-svx routes index and __error -->
+  <slot />
+{/if}
+
+<style>
+  h1,
+  p {
+    text-align: center;
+    display: flex;
+    place-content: center;
+    place-items: center;
+  }
+  a[href='/'] {
+    font-size: 16pt;
+    position: absolute;
+    top: 2em;
+    left: 2em;
+    background-color: rgba(255, 255, 255, 0.1);
+    padding: 1pt 5pt;
+    border-radius: 3pt;
+    transition: 0.2s;
+  }
+  a[href='/']:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
+  nav > a {
+    padding-bottom: 2pt;
+  }
+  nav > a[aria-current='page'] {
+    font-weight: bold;
+  }
+  nav {
+    display: flex;
+    gap: 1ex;
+    place-content: center;
+    margin: 1em 0 3em;
+  }
+</style>
