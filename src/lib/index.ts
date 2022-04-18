746fe1cef772da26d7aba2f006b0db3d7d69d980
GodPingu <44237086+GodPingu@users.noreply.github.com>
2022-04-18 12:35:05 +0100

    Work with string options as is, don't convert to objects internally (#75)
    * Add source of truth and working example
    
    * fix - use backticks in example
    
    * Switch SourceOfTruth to a type instead of an enum
    
    * convert switches to if/elses
    
    * add sourceOfTruth prop to readme
    
    * fix readme being wrong way round
    
    * fix persistence test
    
    * fix hanging bug
    
    * support simple and object options as internal data
    
    previously simple string or number options would have been converted to objects
    
    * document selected{Labels,Values} as read-only
    
    * revert accidental code deletion during merge
    
    Co-authored-by: Janosh Riebesell <janosh.riebesell@gmail.com>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,26 +1,17 @@
 export { default } from './MultiSelect.svelte'
 
-export type Primitive = string | number
+export type Option = string | number | ObjectOption
 
-export type Option = {
-  label: Primitive // text to render to the user for this option
-  value: Primitive // falls back on label if not set
-  title?: string // tooltip to display on hovering this option
+export type ObjectOption = {
+  label: string | number // user-displayed text
+  value: string | number // associated value
+  title?: string // on-hover tooltip
   disabled?: boolean // make this option unselectable
   preselected?: boolean // make this option selected on load (before any user interaction)
   disabledTitle?: string // override the default disabledTitle = 'This option is disabled'
   selectedTitle?: string // tooltip to display when this option is selected and hovered
   [key: string]: unknown // allow any other keys users might want
 }
-
-// a proto option is more flexible than an option but can be auto-converted
-// allows users to pass in simple strings/numbers as options or objects with
-// only labels which are then also used as values
-export type ProtoOption =
-  | Primitive
-  | (Omit<Option, `value`> & {
-      value?: Primitive
-    })
 
 export type DispatchEvents = {
   add: { option: Option }
