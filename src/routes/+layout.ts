138bb989d7469c44691f2afb1245a5d99423ccac
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-10-06 13:36:09 -0700

    rm src/routes/demos.ts, move demo_routes to src/routes/+layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,8 @@
 export const prerender = true
+
+export const demo_routes = Object.keys(import.meta.glob(`./**/*.svx`)).map(
+  (filename) => filename.split(`.`)[1].replace(`/+page`, ``)
+)
+if (demo_routes.length < 5) {
+  throw new Error(`Too few demo routes found: ${demo_routes.length}`)
+}
--- src/routes/demos.ts
+++
@@ -1,6 +0,0 @@
-export const demo_routes = Object.keys(import.meta.glob(`./**/*.svx`)).map(
-  (filename) => filename.split(`.`)[1].replace(`/+page`, ``)
-)
-if (demo_routes.length < 5) {
-  throw `Too few demo routes found: ${demo_routes.length}`
-}
