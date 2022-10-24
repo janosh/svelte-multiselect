767385463be8c0b57c185b23dc60dcd6832a706c
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-10-23 17:10:59 -0700

    Add unit tests for 2-/1-way binding of `activeIndex` and `activeOption` (#139)
    * tests/unit/multiselect.test.ts remove outer describe(`MultiSelect`, () => { ... })
    
    * add test '1-way binding of activeOption and hovering an option makes it active'
    
    * use npm to publish package in CI and run node v18
    
    * readme link to type Option src
    
    * add test '2-way binding of activeIndex'

--- tests/unit/Test2WayBind.svelte
+++ tests/unit/Test2WayBind.svelte
@@ -2,16 +2,27 @@
   import MultiSelect, { type Option } from '$lib'
   import { createEventDispatcher } from 'svelte'
 
-  export let selected: Option[] = []
-  export let options: Option[] = [1, 2, 3]
-  export let value: Option | Option[] | null = null
+  export let activeIndex: number | null = null
+  export let activeOption: Option | null = null
   export let maxSelect: number | null = null
+  export let options: Option[]
+  export let selected: Option[] = []
+  export let value: Option | Option[] | null = null
 
   const dispatch = createEventDispatcher()
 
+  $: dispatch(`activeIndex-changed`, activeIndex)
+  $: dispatch(`activeOption-changed`, activeOption)
   $: dispatch(`options-changed`, options)
   $: dispatch(`selected-changed`, selected)
   $: dispatch(`value-changed`, value)
 </script>
 
-<MultiSelect bind:options bind:selected bind:value {maxSelect} />
+<MultiSelect
+  {maxSelect}
+  bind:activeIndex
+  bind:activeOption
+  bind:options
+  bind:selected
+  bind:value
+/>
