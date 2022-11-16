dfb61f8f852f37a03960af3762b925653581d69f
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-11-15 21:07:28 -0800

    Form validation docs (#159)
    * add test 'can remove user-created selected option which is not in dropdown list'
    
    try fix build & publish step in test.yml workflow
    
    * /form example and add link to upcoming /kit-form-actions example
    
    * add src/routes/kit-form-actions/+page.{svelte, server.ts}
    
    * include new example in example nav
    
    * make CollapsibleCode more versatile to use in /kit-form-action example

---
+++ src/routes/kit-form-actions/+page.server.ts
@@ -0,0 +1,41 @@
+import { invalid } from '@sveltejs/kit'
+import { readFileSync } from 'fs'
+import type { Actions, PageServerLoad } from './$types'
+
+// remove leading underscore to activate this example
+// needs to be disabled for building static site
+export const _actions: Actions = {
+  'validate-form': async ({ request }) => {
+    const data = await request.formData()
+    let colors = data.get(`colors`)
+
+    try {
+      colors = JSON.parse(colors)
+    } catch (err) {
+      return invalid(400, { colors, error: `json` })
+    }
+
+    if (!Array.isArray(colors)) {
+      return invalid(400, { colors, error: `array` })
+    }
+    if (colors.length === 1 && colors[0] === `Red`) {
+      return invalid(400, { colors, error: `boring` })
+    }
+
+    return { colors, success: true }
+  },
+}
+
+export const load: PageServerLoad = () => {
+  return {
+    codes: [`.svelte`, `.server.ts`].map((ext) => {
+      const filepath = new URL(`./+page${ext}`, import.meta.url)
+      try {
+        const code = readFileSync(filepath, `utf8`)
+        return [`+page${ext}`, code]
+      } catch (error) {
+        return [`+page${ext}`, `code`]
+      }
+    }),
+  }
+}
---
+++ src/routes/kit-form-actions/+page.svelte
@@ -0,0 +1,115 @@
+<script lang="ts">
+  import MultiSelect from '$lib'
+  import ColorSlot from '$src/components/ColorSlot.svelte'
+  import { colors } from '$src/options'
+  import CollapsibleCode from '$src/components/CollapsibleCode.svelte'
+  import type { ActionData, PageServerData } from './$types'
+  import { repository } from '../../../package.json'
+
+  export let form: ActionData
+  export let data: PageServerData
+
+  $: err_msg = {
+    json: `The email field is required`,
+    array: `The email field is required`,
+    boring: `Boring answer!`,
+  }[form?.error as string]
+</script>
+
+<p>
+  This example shows the SvelteKit form action way of handling MultiSelect fields in form
+  submission events. If you're not interested in
+  <a href="https://kit.svelte.dev/docs/form-actions#progressive-enhancement">
+    progressively enhanced forms
+  </a>
+  (i.e. supporting no-JS browsers) take a look at the
+  <a href="/form">JS form example</a>
+  instead.
+</p>
+
+<blockquote>
+  Note that this example will only work when running the dev server locally since it needs
+  a server to respond to the form's POST request and this documentation site only serves
+  static HTML.
+</blockquote>
+
+<form method="POST" action="?/validate-form">
+  <label for="colors">
+    <strong>Which colors would you pick for the Martian flag?</strong>
+  </label>
+  <MultiSelect
+    id="colors"
+    options={colors}
+    placeholder="Pick some colors..."
+    name="colors"
+    required
+    invalid={!!form?.error}
+    selected={form?.colors ?? [`Red`]}
+  >
+    <ColorSlot let:idx {idx} let:option {option} slot="selected" />
+    <ColorSlot let:idx {idx} let:option {option} slot="option" />
+  </MultiSelect>
+  <button>Submit</button>
+  <small>
+    select some options, then click submit to see what data MultiSelect sends to a form
+    submit handler
+  </small>
+  {#if err_msg}
+    <p class="error">{err_msg}</p>
+  {/if}
+  {#if form?.success}
+    <p class="success">
+      Good answer! You entered
+      {#each form.colors as color}
+        <ColorSlot
+          option={color}
+          style="display: inline-flex; vertical-align: middle; margin: 0 0 0 1ex;"
+        />
+      {/each}
+    </p>
+  {/if}
+</form>
+
+{#each data.codes as [title, code]}
+  {@const filepath = new URL(import.meta.url).pathname.replace(/\+page.+/, title)}
+  <section>
+    <strong>{title}</strong>
+    <CollapsibleCode
+      {code}
+      language={title.endsWith(`.ts`) ? `typescript` : `html`}
+      github_url="{repository}/blob/main{filepath}"
+    />
+  </section>
+{/each}
+
+<style>
+  form {
+    background-color: rgba(255, 255, 255, 0.1);
+    padding: 1ex 1em;
+    border-radius: 3pt;
+  }
+  p {
+    margin: 1em 0 1ex;
+  }
+  p.error {
+    color: red;
+  }
+  p.success {
+    width: max-content;
+    padding: 1pt 6pt;
+    box-sizing: border-box;
+    color: lightgreen;
+    border: 1px solid;
+    border-radius: 3pt;
+  }
+  section {
+    position: relative;
+    margin: 1em 0;
+    background-color: rgba(0, 255, 255, 0.1);
+    padding: 1ex 1em;
+    border-radius: 3pt;
+  }
+  section strong {
+    padding: 0 6pt 0 0;
+  }
+</style>
