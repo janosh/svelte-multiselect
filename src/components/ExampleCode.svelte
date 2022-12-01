8f6f53b4c64a3448ccb56d5ebe46e85729e39058
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-11-30 21:22:14 -0800

    add src/components/ExampleCode.svelte

--- src/components/ExampleCode.svelte
+++ src/components/ExampleCode.svelte
@@ -1,3 +1,22 @@
+<script>
+  import { onMount } from 'svelte'
+  import { name } from '../../package.json'
+
+  let code
+
+  // replace $lib with 'name' in code
+  onMount(() => {
+    code.innerHTML = code.innerHTML.replace(/\$lib/g, name)
+  })
+</script>
+
 <slot name="example" />
 
-<pre><code><slot name="code" /></code></pre>
+<pre><code bind:this={code}><slot name="code" /></code></pre>
+
+<style>
+  pre {
+    margin-top: 2em;
+    background-color: rgba(255, 255, 255, 0.05);
+  }
+</style>
