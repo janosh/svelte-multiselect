7ee60481e88e39d484da7c7bdccc61e0fc8ad00a
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-01 08:28:50 -0800

    add CopyButton to ExampleCode.svelte

--- src/components/ExampleCode.svelte
+++ src/components/ExampleCode.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from 'svelte'
   import { name } from '../../package.json'
+  import CopyButton from './CopyButton.svelte'
 
   let code
 
@@ -12,11 +13,21 @@
 
 <slot name="example" />
 
-<pre><code bind:this={code}><slot name="code" /></code></pre>
+<pre><aside>
+  <CopyButton content={code} />
+</aside><code bind:this={code}><slot name="code" /></code></pre>
 
 <style>
   pre {
     margin-top: 2em;
     background-color: rgba(255, 255, 255, 0.05);
+    position: relative;
+  }
+  aside {
+    position: absolute;
+    top: 1em;
+    right: 1em;
+    display: flex;
+    gap: 1em;
   }
 </style>
