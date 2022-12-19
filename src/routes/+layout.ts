e9b27e160b12f72bec7eb6145d0ee5643bf93ae7
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-18 19:45:24 -0600

    redirect {changelog,contributing}.md to {changelog,contributing}
    fixes markdown-link-check error ./src/routes/contributing.md is a directory! Please provide a valid filename as an argument.

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,11 +1,20 @@
+import { redirect } from '@sveltejs/kit'
+import type { LayoutLoad } from './$types'
+
 export const prerender = true
 
 export const _demo_routes = Object.keys(
   import.meta.glob(`./*/+page.{svx,svelte}`)
 )
   .map((filename) => filename.split(`/`)[1])
-  .filter((name) => !name.endsWith(`.md`))
+  .filter((name) => ![`contributing`, `changelog`].includes(name))
 
 if (_demo_routes.length < 5) {
   throw new Error(`Too few demo routes found: ${_demo_routes.length}`)
+}
+
+export const load: LayoutLoad = ({ url }) => {
+  if (url.pathname.endsWith(`.md`)) {
+    throw redirect(307, url.pathname.replace(/\.md$/, ``))
+  }
 }
