ef4d669ff548fa989a752cd767d342a7168a4255
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-12-28 13:50:55 -0800

    v8.2.3

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: `pnpm dev --port 3005`,
+    command: `vite dev --port 3005`,
     port: 3005,
   },
 }
