f24e0256fcdc32c90ed798edbb663a6be18ebe00
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-20 18:38:02 -0800

    breaking: rename addOptionMsg to createOptionMsg
    add Footer to demo pages with links to 'edit page' and 'multiselect showcase'
    
    remove some not so recent breaking changes from readme

--- src/site/index.ts
+++ src/site/index.ts
@@ -1,6 +1,7 @@
 export { default as ColorSlot } from './ColorSlot.svelte'
 export { default as DemoNav } from './DemoNav.svelte'
 export { default as Examples } from './Examples.svx'
+export { default as Footer } from './Footer.svelte'
 export { default as IconifySlot } from './IconifySlot.svelte'
 export { default as LanguageSlot } from './LanguageSlot.svelte'
 export { default as MinusIcon } from './MinusIcon.svelte'
