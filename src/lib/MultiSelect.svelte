d69ab25f9e67a222df1aafd9d026431b66561ce0
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-01-25 19:43:44 -0800

    log error in get_label() if option is object but has no label key

--- src/lib/MultiSelect.svelte
+++ src/lib/MultiSelect.svelte
@@ -71,7 +71,17 @@
   export let value: Option | Option[] | null = null
 
   // get the label key from an option object or the option itself if it's a string or number
-  const get_label = (op: GenericOption) => (op instanceof Object ? op.label : op)
+  const get_label = (op: GenericOption) => {
+    if (op instanceof Object) {
+      if (op.label === undefined) {
+        console.error(
+          `MultiSelect option ${JSON.stringify(op)} is an object but has no label key`
+        )
+      }
+      return op.label
+    }
+    return op
+  }
 
   // if maxSelect=1, value is the single item in selected (or null if selected is empty)
   // this solves both https://github.com/janosh/svelte-multiselect/issues/86 and
