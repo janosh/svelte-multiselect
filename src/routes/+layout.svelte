420bb96e122ab2959610672619266f4f8a275d52
Janosh Riebesell <janosh.riebesell@gmail.com>
2025-06-13 07:32:36 -0400

    update package.json deps and refactor demo navigation
    - drop writable store for demo navigation, use `demo_pages` array from newly created (demoos)/index.ts
    - simplify code fence buttons in +layout.svelte with <CopyButton global />
    - update persistent demo page to use sessionStorage directly, not through persisted_store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,50 +1,19 @@
 <script lang="ts">
-  import { afterNavigate, goto } from '$app/navigation'
+  import { goto } from '$app/navigation'
   import { page } from '$app/state'
   import { CmdPalette } from '$lib'
   import { repository } from '$root/package.json'
   import { Footer } from '$site'
-  import { demos } from '$site/stores'
-  import { mount, type Snippet } from 'svelte'
+  import { type Snippet } from 'svelte'
   import Toc from 'svelte-toc'
   import { CopyButton, GitHubCorner } from 'svelte-zoo'
   import '../app.css'
+  import { routes } from './(demos)'
 
   interface Props {
     children?: Snippet
   }
   let { children }: Props = $props()
-
-  afterNavigate(() => {
-    for (const node of document.querySelectorAll(`pre > code`)) {
-      // skip if <pre> already contains a button (presumably for copy)
-      const pre = node.parentElement
-      if (!pre || pre.querySelector(`button`)) continue
-
-      mount(CopyButton, {
-        target: pre,
-        props: {
-          content: node.textContent ?? ``,
-          style: `position: absolute; top: 1ex; right: 1ex;`,
-        },
-      })
-    }
-  })
-
-  const routes = Object.keys(import.meta.glob(`./**/+page.{svx,svelte,md}`)).map(
-    (filename) => {
-      const parts = filename.split(`/`).filter((part) => !part.startsWith(`(`)) // remove hidden route segments
-      return { route: `/${parts.slice(1, -1).join(`/`)}`, filename }
-    },
-  )
-
-  if (routes.length < 3) {
-    console.error(`Too few demo routes found: ${routes.length}`)
-  }
-
-  $demos = routes
-    .filter(({ filename }) => filename.includes(`/(demos)/`))
-    .map(({ route }) => route)
 
   const actions = routes.map(({ route }) => ({ label: route, action: () => goto(route) }))
 </script>
@@ -52,6 +21,8 @@
 <CmdPalette {actions} placeholder="Go to..." />
 
 <GitHubCorner href={repository} />
+
+<CopyButton global />
 
 {#if !page.error && page.url.pathname !== `/`}
   <a href="." aria-label="Back to index page">&laquo; home</a>
@@ -64,11 +35,10 @@
     headingSelector="main > :where(h2, h3)"
     breakpoint={1250}
     --toc-mobile-bg="#1c0e3e"
-    --toc-font-size="9pt"
     --toc-li-padding="3pt 1ex"
     --toc-mobile-btn-color="white"
-    --toc-max-width="20em"
-    --toc-desktop-nav-margin="0 0 0 17em"
+    --toc-desktop-nav-margin="0 0 0 14em"
+    aside_style="max-width: 24em"
   />
 {/if}
 
